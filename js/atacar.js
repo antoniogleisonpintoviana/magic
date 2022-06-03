32ab71aed38210be1b7778c5b03f97a9549de4ae
antoniogleisonpintoviana <31672043+antoniogleisonpintoviana@users.noreply.github.com>
2022-06-03 18:59:55 -0300

    Create atacar.js

---
+++ js/atacar.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+  $('img').click(function(event) {
+    $(this).css('transform','rotate(45deg)');
+    
+  });
+});
+
+$(document).ready(function() {
+  $('img').dblclick(function(event) {
+    $(this).css('transform','rotate(0deg)');
+
+  });
+});
+
+
+                    
+                    
+                
+                
